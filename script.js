29e4bb5ce251901f72d348f931e79eafd86ac10b
iammradioactive <42364197+iammradioactive@users.noreply.github.com>
2020-03-26 12:53:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = "David";
+let courses = ["Html", "CSS", "Javascript", "PHP","C#"];
+
+let profile = "My name is " + name + " and " + courses + " are the courses I'm taking.";
+
+console.log(profile);
+
+for (let i = 1; i <= 200; i++) {
+   if (i%2==0) console.log(i);
+    
+}
+
